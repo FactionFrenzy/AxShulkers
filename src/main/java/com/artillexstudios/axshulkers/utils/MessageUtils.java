f3c66db66baf5f64485802702f426d1663af9595
r59q <andreashenriksen@yahoo.dk>
2024-07-14 13:59:20 +0200

    Return when messages are null

--- src/main/java/com/artillexstudios/axshulkers/utils/MessageUtils.java
+++ src/main/java/com/artillexstudios/axshulkers/utils/MessageUtils.java
@@ -1,9 +1,11 @@
 package com.artillexstudios.axshulkers.utils;
 
+import com.artillexstudios.axshulkers.AxShulkers;
 import org.bukkit.command.CommandSender;
 
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
 
 import static com.artillexstudios.axshulkers.AxShulkers.CONFIG;
 import static com.artillexstudios.axshulkers.AxShulkers.MESSAGES;
@@ -16,6 +18,10 @@ public class MessageUtils {
     }
 
     public static void sendMsgP(CommandSender player, String path, Map<String, String> replacements) {
+        if (MESSAGES == null) {
+            AxShulkers.getInstance().getLogger().log(Level.INFO, "An error occurred while loading messages.");
+            return;
+        }
         if (MESSAGES.getString(path).isEmpty()) return;
         AtomicReference<String> message = new AtomicReference<>(MESSAGES.getString(path));
         replacements.forEach((key, value) -> message.set(message.get().replace(key, value)));
